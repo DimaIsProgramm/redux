4a69b26e84f107ebfc7236f3720580746afe8f71
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-07-20 16:27:01 +0300

    Add files via upload

---
+++ lesson-2/task-3/src/users.reducer.js
@@ -0,0 +1,21 @@
+const initialState = {
+  usersList: [],
+};
+
+export const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_USER':
+      return {
+        ...state,
+        usersList: state.usersList.concat(action.newUser),
+      };
+    case 'DELETE_USER':
+      return {
+        ...state,
+        usersList: state.usersList.slice().filter(user => user.id !== action.id),
+      };
+
+    default:
+      return state;
+  }
+};
