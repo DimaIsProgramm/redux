252305464d8ada30bcf2ed749f99225abe477730
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-07-21 16:49:13 +0300

    Add files via upload

---
+++ lesson-3/task-2/src/index.js
@@ -0,0 +1,9 @@
+import store from './store';
+import { increment, decrement } from './counter.actions';
+
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(increment());
+// store.getState(decrement());
+// store.dispatch(increment());
+// store.dispatch(reset());
