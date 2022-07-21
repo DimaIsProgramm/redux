f73c9c73db077f7135da9eb166b34ff50b45d49d
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-07-21 16:51:26 +0300

    Delete lesson-3/task-2/src directory

--- lesson-3/task-2/src/index.js
+++
@@ -1,9 +0,0 @@
-import store from './store';
-import { increment, decrement } from './counter.actions';
-
-// store.dispatch(increment());
-// store.dispatch(decrement());
-// store.dispatch(increment());
-// store.getState(decrement());
-// store.dispatch(increment());
-// store.dispatch(reset());
